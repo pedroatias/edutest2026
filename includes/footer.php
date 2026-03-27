9947108d59169520268736d21aa1541ea72ae79a
Pedro Atias <pedroatias@jps.com.ve>
2026-03-27 16:46:52 -0500

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</div><!-- end wrapper -->
+<script src="<?= $base_url ?>assets/js/jquery.min.js"></script>
+<script src="<?= $base_url ?>assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $base_url ?>assets/js/sweetalert2.all.min.js"></script>
+<script src="<?= $base_url ?>assets/js/app.js"></script>
+</body>
+</html>
